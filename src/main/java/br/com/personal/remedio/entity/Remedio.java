365901d5cfc8be5c939c49693339ca3dd009d99e
Pedro Dourado Rocha <pdourado2006@gmail.com>
2025-12-25 10:25:17 -0300

    feat (remedio): ADD da entidade remedio e todas as suas camadas

---
+++ src/main/java/br/com/personal/remedio/entity/Remedio.java
@@ -0,0 +1,43 @@
+package br.com.personal.remedio.entity;
+
+import br.com.personal.insfrastructre.role.StatusRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import br.com.personal.insfrastructre.persistenceentity.PersistenceEntity;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "remedio")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Remedio extends PersistenceEntity {
+
+    @Column(nullable = false)
+    String nome;
+
+    @Column(nullable = false)
+    double quantidade;
+
+    @Column(nullable = false)
+    LocalDate proxCompra;
+
+    @Column(nullable = false)
+    double usoDiario;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    StatusRole status;
+
+    /*
+   @Column (nullable = false)
+   @OneToMany(fetch = fetchType.EAGER)
+   Pessoa pessoa;
+     */
+
+}
