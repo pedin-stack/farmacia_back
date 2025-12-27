d2c1377e664bce086ec79a5f63b098f57191bde5
Pedro Dourado Rocha <pdourado2006@gmail.com>
2025-12-27 03:19:19 -0300

    configuração : mudança no corsConfig para aceitar requisições vindas do front hospedado

--- src/main/java/br/com/personal/insfrastructre/config/CorsConfig/CorsConfig.java
+++ src/main/java/br/com/personal/insfrastructre/config/CorsConfig/CorsConfig.java
@@ -6,19 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-
-                // Permite os métodos necessários
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-
-                // Permite todos os cabeçalhos
-                .allowedHeaders("*");
-
-        // ESSENCIAL: Permite cookies/sessão
-        //.allowCredentials(true); ADICIONAR DEPOIS
+                .allowedOrigins("https://farmacia-beryl.vercel.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
